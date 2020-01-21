cd40d6dd1f399c9344e318c8b986a207c98101cf
Jhon Guerrero <jhoguer@gmail.com>
2020-01-21 16:59:24 -0500

    Entendiendo Globals

---
+++ modulos/globales.js
@@ -0,0 +1,20 @@
+// let i = 0;
+// let intervalo = setInterval(() => {
+//   console.log('Hola');
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+// }, 1000);
+
+
+// console.log(353553);
+// console.log(353553);
+// console.log(353553);
+// console.log(353553);
+
+// console.log(process);
+console.log(__filename);
+console.log(__dirname);
+globalThis.miVariable = 'ElValorDeLaVariableGlobal';
+console.log(miVariable);
